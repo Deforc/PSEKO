a8686605fbadc92e39c04c6f5491cf5915d8d56d
Brendow271 <pashashkuropat@yandex.ru>
2025-04-08 10:03:34 +0300

    Update grammar/uml

--- GUI/frontend/src/components/Header.vue
+++ GUI/frontend/src/components/Header.vue
@@ -2,7 +2,7 @@
   <v-app-bar color="green" dark app>
     <v-btn @click="showImage('grammar')">Show Grammar</v-btn>
     <v-btn @click="showImage('uml')">Show UML</v-btn>
-    <v-dialog v-model="dialog" max-width="70%">
+    <v-dialog v-model="dialog" min-width="80%">
       <v-card>
         <v-img :src="imageSrc" contain></v-img>
       </v-card>
